bc816ac551ddb1947a11eda644209f430dea07b9
SSen <i@ssen.name>
2020-04-05 22:57:51 +0900

    chore: remove useless package

--- src/_packages/@ssen/dashboard-components/components/HierarchyList.tsx
+++
@@ -1,4 +0,0 @@
-import { HierarchyList as _HierarchyList } from '@ssen/hierarchy-list';
-import styled from 'styled-components';
-
-export const HierarchyList = styled(_HierarchyList).attrs(({ openProvider }) => ({}));
